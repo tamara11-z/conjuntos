0cb874d5d6bd3167ece93afff38691b0d886bcbe
tamara11-z <Tamara.Zegarra0226@jala.university>
2026-09-17 10:27:45 -0400

    Fix message formatting in script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function seleccionar(materia) {
     const resultado = document.getElementById("resultado");
 
     resultado.innerHTML =
-        "✅ Has seleccionado <strong>" +
+        " Has seleccionado <strong>" +
         materia +
         "</strong>. Esta respuesta se utilizará para formar los conjuntos.";
 }
