d11a5df558886996367e392257467970b0631f53
tamara11-z <Tamara.Zegarra0226@jala.university>
2026-09-17 10:18:00 -0400

    Add function to display selected subject

---
+++ script.js
@@ -0,0 +1,11 @@
+```javascript
+function seleccionar(materia) {
+
+    const resultado = document.getElementById("resultado");
+
+    resultado.innerHTML =
+        "✅ Has seleccionado <strong>" +
+        materia +
+        "</strong>. Esta respuesta se utilizará para formar los conjuntos.";
+}
+```
